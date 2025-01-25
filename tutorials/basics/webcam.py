694372946a4cd6b63a468a55529df01b034c4532
zachcroft <zcroft@umich.edu>
2025-01-24 22:45:31 -0500

    Add script for taking webcam pictures

---
+++ tutorials/basics/webcam.py
@@ -0,0 +1,24 @@
+#===
+# This script uses your webcam to take pictures
+# continuously given a framerate
+#===
+import cv2
+
+# Create a video capture object
+webcam = cv2.VideoCapture(0)
+
+# Choose a framerate
+fps    = 60
+period = int(1000 / fps)
+
+ret = True
+while ret:
+    # Capture a frame
+    ret, frame = webcam.read()
+
+    # Display the frame every 100 msec
+    cv2.imshow("Webcam capture", frame)
+    cv2.waitKey(period)
+
+# Release the object
+webcam.release()
